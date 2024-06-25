47bd914f55ac809ee92ebb209dc531a414f59e4f
Praveen Lohar <praveenlohar.in@gmail.com>
2024-06-25 18:11:27 +0530

    chore : added chapters page and form in admin

--- src/app/admin/chapter/page.js
+++ src/app/admin/chapter/page.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import styles from "./chapter.module.scss";
+import ChapterForm from "../../../components/Admin/Chapter/ChapterForm";
 const page = () => {
-  return <div>Chapter</div>;
+  return (
+    <div className={styles.coursePageWrapper}>
+      <div className={styles.coursePageWrapper__formWrapper}>
+        <ChapterForm />
+      </div>
+    </div>
+  );
 };
 
 export default page;
