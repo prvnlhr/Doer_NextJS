a7d34bda63b755ff0aef91d5b5e616a8b5dcd57d
Praveen Lohar <praveenlohar.in@gmail.com>
2024-07-24 20:47:02 +0530

    chore : code cleanup

--- src/app/(public)/user/[userId]/classroom/@stats/page.js
+++ src/app/(public)/user/[userId]/classroom/@stats/page.js
@@ -5,7 +5,6 @@ import React from "react";
 const page = async ({ params }) => {
   const { userId } = params;
   const stats = await fetchUsersStatsData(userId);
-  console.log("stats", stats);
   return <Stats stats={stats} />;
 };
 
