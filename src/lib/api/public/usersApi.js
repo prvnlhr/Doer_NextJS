46acd2bcbd21d303524f8da26564f198fbe85d75
Praveen Lohar <praveenlohar.in@gmail.com>
2024-07-24 14:07:39 +0530

    chore : update time spending logic

--- src/lib/api/public/usersApi.js
+++ src/lib/api/public/usersApi.js
@@ -1,7 +1,7 @@
 import { revalidatePathHandler, revalidateTagHandler } from "@/app/revalidate";
 
-// const BASE_URL = process.env.BASE_URL || "https://doer-next.vercel.app";
-const BASE_URL = process.env.BASE_URL || "http://127.0.0.1:3000";
+const BASE_URL = process.env.BASE_URL || "https://doer-next.vercel.app";
+// const BASE_URL = process.env.BASE_URL || "http://127.0.0.1:3000";
 
 export async function fetchUserData(userId) {
   try {
