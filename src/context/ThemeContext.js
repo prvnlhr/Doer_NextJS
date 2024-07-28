27a0572aae13871d14199269b0d24490d1100e6a
Praveen Lohar <praveenlohar.in@gmail.com>
2024-07-28 21:19:52 +0530

    chore : persisted theme in localstorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,7 +3,13 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("dark");
+  // Initialize the theme state from localStorage or default to "light"
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") || "light";
+    }
+    return "light";
+  });
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
@@ -13,6 +19,11 @@ export const ThemeProvider = ({ children }) => {
     const bodyClassList = document.body.classList;
     bodyClassList.remove("theme--light", "theme--dark");
     bodyClassList.add(`theme--${theme}`);
+
+    // Save the theme state to localStorage whenever it changes
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", theme);
+    }
   }, [theme]);
 
   return (
